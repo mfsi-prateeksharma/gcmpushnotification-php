e70b97a23fa5224afb5ed9e9a95d99e39ae46e4f
prateek <prateek.sharma@mindfiresolutions.com>
2016-01-22 13:19:12 +0530

    Initial commit

---
+++ gcm_server_php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "testinggcm");
+define("DB_PASSWORD", "testgcm");
+define("DB_DATABASE", "test_gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyCEx9vUBJ1mSD-tVXdgtOIEPXdJaqlfzeo"); // Place your Google API Key
+?>
